0719f27ccb33e0b97d2a0a15817394fb153d678f
Jitendra Kumar Yadav <j.yadav@tu-braunschweig.de>
2020-08-17 12:00:35 +0200

    commit

--- src/myAwt/First.java
+++ src/myAwt/First.java
@@ -5,7 +5,6 @@ import java.awt.*;
 public class First extends Frame {
     public First() throws HeadlessException {
         Button b=new Button("click me");
-
         b.setBounds(30,100,80,30);// setting button position
         add(b);//adding button into frame
         setSize(300,300);//frame size 300 width and 300 height
