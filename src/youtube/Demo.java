aff6b0d77f80b8257b9c96ce451fe0c7f1ff4625
Jitendra Kumar Yadav <j.yadav@tu-braunschweig.de>
2020-09-24 21:30:13 +0200

    update

--- src/youtube/Demo.java
+++ src/youtube/Demo.java
@@ -3,8 +3,6 @@ package youtube;
 import java.awt.*;
 
 public class Demo extends Frame {
-    String name;
-
     public Demo(String name) {
         super(name);
         setSize(800,500);
@@ -15,17 +13,22 @@ public class Demo extends Frame {
 
     public void paint(Graphics g){
         g.setColor(Color.white);
-        int x =150, y =150, w =150, h =150, sa =0, ma=3;
+        int x = 150;
+        int y = 150;
+        int w = 150;
+        int h = 150;
+        int sa = 0;
+        int ma = 3;
 
-        for(int i=1;;i++){
-            int r1 = (int) (Math.random()*255);
-            int g1 = (int) (Math.random()*255);
-            int b1 = (int) (Math.random()*255);
+        for (int i = 1; ; i++) {
+            int r1 = (int) (Math.random() * 255);
+            int g1 = (int) (Math.random() * 255);
+            int b1 = (int) (Math.random() * 255);
 
-            Color c = new Color(r1,g1,b1);
+            Color c = new Color(r1, g1, b1);
             g.setColor(c);
-           g.fillArc(x,y,w,h,sa,ma);
-           sa +=12;
+            g.fillArc(x, y, w, h, sa, ma);
+            sa += 12;
 
            try {
                 Thread.sleep(200);
